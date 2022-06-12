4f40766c8bfe70ebda8f05dbef2c90a244aa31a2
Pablo Gagliardi <pablogagliardi91@gmail.com>
2022-06-12 11:56:45 -0300

    agregando módulo para cargar datos de las vacunas aplicadas

--- src/routes/vaccines.js
+++ src/routes/vaccines.js
@@ -9,32 +9,32 @@ const { ADMINISTRADOR } = require('../helpers/Roles');
 
 router.get('/vaccines/add', isAuthenticated, async (req, res) => {
     const user = await User.findById(req.user.id).lean();
-    res.render('vaccines/new-vaccines', {isAdmin: user.role === ADMINISTRADOR});
+    res.render('vaccines/new-vaccines', { isAdmin: user.role === ADMINISTRADOR });
 });
 
 router.post('/vaccines/new-vaccines', isAuthenticated, async (req, res) => {
     const { name, date } = req.body;
     console.log(name, date);
     const errors = [];
-    if (!name){
-        errors.push({text: 'Debe elegir una vacuna'});
-    
+    if (!name) {
+        errors.push({ text: 'Debe elegir una vacuna' });
+
     } else {
         console.log(req.user.id);
-        const vaccineName = await Vaccine.findOne( { name: name, user: req.user.id}); 
-        if (vaccineName){
+        const vaccineName = await Vaccine.findOne({ name: name, user: req.user.id });
+        if (vaccineName) {
             console.log('vacuna ya registrada');
             req.flash('error', 'La vacuna ya se encuentra registrada');
-            res.redirect('/vaccines'); 
-        }else {
-            const newVaccine = new Vaccine({name, date});
+            res.redirect('/vaccines');
+        } else {
+            const newVaccine = new Vaccine({ name, date });
             newVaccine.user = req.user.id;
-            newVaccine.place = null; 
-            newVaccine.lot = null; 
+            newVaccine.place = null;
+            newVaccine.lot = null;
             newVaccine.labName = null;
             await newVaccine.save();
             req.flash('success_msg', 'Se ha registrado una nueva vacuna.');
-            res.redirect('/vaccines'); 
+            res.redirect('/vaccines');
         }
 
     }
@@ -43,43 +43,43 @@ router.post('/vaccines/new-vaccines', isAuthenticated, async (req, res) => {
 router.get('/vaccines', isAuthenticated, async (req, res) => {
     const user = await User.findById(req.user.id).lean();
     if (user.role === ADMINISTRADOR) {
-        const vaccines = await Vaccine.find({}).lean().sort({date: 'desc'});
+        const vaccines = await Vaccine.find({}).lean().sort({ date: 'desc' });
         return res.render('vaccines/all-vaccines', { vaccines });
     }
-    const vaccines = await Vaccine.find({user: req.user.id}).lean().sort({date: 'desc'});
+    const vaccines = await Vaccine.find({ user: req.user.id }).lean().sort({ date: 'desc' });
     res.render('vaccines/all-vaccines', { vaccines });
 });
 
 router.get('/users/miperfil', isAuthenticated, async (req, res) => {
-    const usuarios = await User.find({email: req.user.email}).lean();
-    res.render('users/miperfil', {usuarios});
+    const usuarios = await User.find({ email: req.user.email }).lean();
+    res.render('users/miperfil', { usuarios });
 });
 
 router.get('/users/miperfil/edit/:id', isAuthenticated, async (req, res) => {
     const usuari = await User.findById(req.params.id).lean();
     console.log(req.params.id);
-    res.render('users/edit', {usuari});
-}); 
+    res.render('users/edit', { usuari });
+});
 
 router.put('/users/miperfil/edit/:id', isAuthenticated, async (req, res) => {
-    const { name, email }= req.body;
-    const us = await User.findByIdAndUpdate(req.params.id, {name, email });
+    const { name, email } = req.body;
+    const us = await User.findByIdAndUpdate(req.params.id, { name, email });
     req.flash('success_msg', 'Datos actualizados correctamente');
     res.redirect('/users/miperfil');
-}); 
+});
 
 
 router.get('/vaccines/edit/:id', isAuthenticated, async (req, res) => {
     const vaccine = await Vaccine.findById(req.params.id).lean();
-    res.render('vaccines/edit-vaccine', {vaccine});
-}); 
+    res.render('vaccines/edit-vaccine', { vaccine });
+});
 
 router.put('/vaccines/edit-vaccine/:id', isAuthenticated, async (req, res) => {
-    const { date }= req.body;
-    await Vaccine.findByIdAndUpdate(req.params.id, {date});
+    const { date } = req.body;
+    await Vaccine.findByIdAndUpdate(req.params.id, { date });
     req.flash('success_msg', 'Vacuna actualizada correctamente');
     res.redirect('../');
-}); 
+});
 
 router.delete('/vaccines/delete/:id', isAuthenticated, async (req, res) => {
     await Vaccine.findByIdAndDelete(req.params.id);
@@ -87,22 +87,29 @@ router.delete('/vaccines/delete/:id', isAuthenticated, async (req, res) => {
     res.redirect('../');
 });
 
-
 //aplicar vacuna
 router.post('/vaccines/aplicarvacuna/:id', isAuthenticated, async (req, res) => {
     console.log(req.params.id);
     const paciente = await User.findById(req.params.id);
     console.log(paciente);
     const newVaccine = new Vaccine();
-            newVaccine.name = paciente.name;
-            newVaccine.user = req.params.id;
-            newVaccine.date = Date.now();
-            newVaccine.place = null; 
-            newVaccine.lot = null; 
-            newVaccine.labName = null;
-            await newVaccine.save();
-            req.flash('success_msg', 'La vacuna ha sido aplicada.');
-            res.redirect('/users/vacunador/selector-sede');
-}); 
+    newVaccine.name = paciente.name;
+    newVaccine.user = req.params.id;
+    newVaccine.date = Date.now();
+    newVaccine.place = null; //esta sede
+    newVaccine.lot = null;
+    newVaccine.labName = null;
+    await newVaccine.save();
+    req.flash('success_msg', 'La vacuna ha sido aplicada.');
+    res.render('./users/vacunador/cargar-datos-vacuna', { newVaccine });
+});
+
+router.put('/users/vacunador/cargar-datos-vacuna/:id', isAuthenticated, async (req, res) => {
+    const { name, labName, lot, vaccinator, place } = req.body;
+    const vacuna = await Vaccine.findByIdAndUpdate(req.params.id, { name: name, labName: labName, lot: lot, vaccinator: vaccinator, place: place });
+    req.flash('success_msg', 'Se guardaron los datos de la vacuna');
+    res.redirect('/users/vacunador/selector-sede');
+
+});
 
 module.exports = router;
